4ab56beb36f55bc401c8a2f876d6f515d3bdbda9
Riya Tyagi <riyatyagi284@gmail.com>
2023-08-05 22:27:50 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Define custom chunks based on your application's needs
+          // For example, separate large dependencies into a vendor chunk
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+          // Separate other specific modules into different chunks if necessary
+          // Add more conditions as needed
+        },
+      },
+    },
+    // Adjust the chunk size warning limit (in bytes)
+    chunkSizeWarningLimit: 1000000,
+  },
+});
+
